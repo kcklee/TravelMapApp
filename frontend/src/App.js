9ad580ced7bbe85e3599f126236308c470025565
kcklee <kevlee_90@hotmail.com>
2023-04-10 16:09:47 -0700

    Connected backend to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Map, {NavigationControl, Marker, Popup} from 'react-map-gl';
 import Navbar from './components/navbar.js';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 import {Star} from "@mui/icons-material";
+import axios from 'axios';
 
 function App() {
+  const [pins, setPins] = useState([]);
   const [showPopup, setShowPopup] = React.useState(true);
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch(err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, [])
   return (
     <div className="App">
       <Navbar/>
@@ -21,18 +36,20 @@ function App() {
         mapStyle="https://api.maptiler.com/maps/streets/style.json?key=78hZLzofApxPlD694BeZ"
       >
         <NavigationControl position="top-left" />
-        <Marker longitude = {2.294694}
-        latitude = {48.858093}/>
+        {pins.map( p => (
+          <>
+        <Marker longitude = {p.long}
+        latitude = {p.lat}/>
     {showPopup && (
       <Popup 
-        longitude={2.294694} latitude={48.858093}
+        longitude={p.long} latitude={p.lat}
         anchor="top"
         onClose={() => setShowPopup(false)}>
         <div className = "card">
           <label>Place</label>
-          <h4 className="place">Eiffel Tower</h4>
+          <h4 className="place">{p.title}</h4>
           <label>Review</label>
-          <p className = "desc">Beautiful Place!</p>
+          <p className = "desc">{p.desc}</p>
           <label>Rating</label>
             <div className="stars">
               <Star className="star"></Star>
@@ -42,10 +59,12 @@ function App() {
               <Star className="star"></Star>
             </div>
           <label>Information</label>
-          <span className="username">Created by <b> Kevin</b></span>
+          <span className="username">Created by <b> {p.username}</b></span>
           <span className="date">1 hour ago</span>
         </div>
-      </Popup>)}
+      </Popup>)}</>
+        ))}
+
       </Map>
     </div>
   );
